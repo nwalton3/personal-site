552ae35078c004a3e405c8d54bf0d695e9a8a913
Nathan Walton <nathan.walton@ldschurch.org>
2016-12-01 08:30:30 -0700

    Initial update

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+/* gulpfile.js */
+
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var pug = require('gulp-pug');
+var changed = require('gulp-changed');
+var connect = require('gulp-connect');
+var plumber = require('gulp-plumber');
+var data = require('gulp-data');
+var ext_replace = require('gulp-ext-replace');
+var livereload = require('gulp-livereload');
+var clean = require('gulp-clean');
+var rename = require("gulp-rename");
+
+
+// Compile Jade
+gulp.task('pug', ['page-index','page-portfolio','page-students']);
+
+gulp.task('page-index', function() {
+    return gulp.src('pug/index.html.jade')
+        .pipe(plumber())
+        .pipe(data( function(file){ return require('./data/page/index.json'); } ))
+        .pipe(pug({ pretty: true }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(livereload());
+});
+
+gulp.task('page-portfolio', function() {
+    return gulp.src('pug/portfolio.html.jade')
+        .pipe(plumber())
+        .pipe(data( function(file){ return require('./data/page/portfolio.json'); } ))
+        .pipe(pug({ pretty: true }))
+        .pipe(rename('portfolio.html'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(livereload());
+});
+
+gulp.task('page-students', function() {
+    return gulp.src('pug/portfolio.html.jade')
+        .pipe(plumber())
+        .pipe(data( function(file){ return require('./data/page/students.json'); } ))
+        .pipe(pug({ pretty: true }))
+        .pipe(rename('students.html'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(livereload());
+});
+
+
+// Compile Sass
+gulp.task('sass', function() {
+    return gulp.src('sass/*.sass')
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(livereload());;
+});
+
+// Lint JS
+gulp.task('lint', function() {
+    return gulp.src('script.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Minify JS
+gulp.task('uglify', function () {
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(ext_replace('.min.js', '.js'))
+        .pipe(livereload());
+});
+
+// // Concatenate & Minify JS
+// gulp.task('scripts', function() {
+//     return gulp.src('js/*.js')
+//         .pipe(concat('all.js'))
+//         .pipe(gulp.dest('dist'))
+//         .pipe(rename('all.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist'));
+// });
+
+// Connect server
+gulp.task('connect', function() {
+  connect.server({
+    port: 9006,
+    root: 'dist'
+  });
+});
+
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch(['**/*.sass'], ['sass']);
+    gulp.watch(['**/*.jade'], ['pug']);
+    gulp.watch(['js/*.js'], ['lint', 'uglify']);
+});
+
+// Default Task
+gulp.task('default', ['connect', 'sass', 'pug', 'lint', 'uglify', 'watch']);
+
+
