66228964da0a58938ab5de0c5129a40d686ae2a3
nwalton3 <nwalton@byu.edu>
2016-12-28 12:48:30 -0700

    Add pug for pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,32 @@
 var gulp = require('gulp'); 
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+// var jshint = require('gulp-jshint');
+// var sass = require('gulp-sass');
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
 var pug = require('gulp-pug');
-var changed = require('gulp-changed');
-var connect = require('gulp-connect');
+// var changed = require('gulp-changed');
+// var connect = require('gulp-connect');
 var plumber = require('gulp-plumber');
-var data = require('gulp-data');
+// var data = require('gulp-data');
 var ext_replace = require('gulp-ext-replace');
-var livereload = require('gulp-livereload');
-var clean = require('gulp-clean');
-var rename = require("gulp-rename");
+// var livereload = require('gulp-livereload');
+// var clean = require('gulp-clean');
+// var rename = require("gulp-rename");
 
 
 // Compile Pug
 gulp.task( 'pug', function(){
-    return gulp.src(['./pug/*.html.pug', './pug/*.html.jade'])
+    return gulp.src(['./_pages/*.html.pug'])
         .pipe(plumber())
         .pipe(pug({
             pretty: true,
             env: "local",
             assets: ""}))
         .pipe(ext_replace('.html', '.html.html'))
-        .pipe(gulp.dest('./dist/'))
-        .pipe(livereload());
+        .pipe(gulp.dest('./pages/'));
 });
 
 
@@ -67,30 +66,30 @@ gulp.task( 'pug', function(){
 // });
 
 
-// Compile Sass
-gulp.task('sass', function() {
-    return gulp.src('sass/*.sass')
-        .pipe(plumber())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(livereload());;
-});
+// // Compile Sass
+// gulp.task('sass', function() {
+//     return gulp.src('sass/*.sass')
+//         .pipe(plumber())
+//         .pipe(sass({outputStyle: 'compressed'}))
+//         .pipe(gulp.dest('./dist/css/'))
+//         .pipe(livereload());;
+// });
 
-// Lint JS
-gulp.task('lint', function() {
-    return gulp.src('script.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
+// // Lint JS
+// gulp.task('lint', function() {
+//     return gulp.src('script.js')
+//         .pipe(jshint())
+//         .pipe(jshint.reporter('default'));
+// });
 
-// Minify JS
-gulp.task('uglify', function () {
-    return gulp.src('js/*.js')
-        .pipe(uglify())
-        .pipe(ext_replace('.min.js', '.js'))
-        .pipe(gulp.dest('./dist/js/'))
-        .pipe(livereload());
-});
+// // Minify JS
+// gulp.task('uglify', function () {
+//     return gulp.src('js/*.js')
+//         .pipe(uglify())
+//         .pipe(ext_replace('.min.js', '.js'))
+//         .pipe(gulp.dest('./dist/js/'))
+//         .pipe(livereload());
+// });
 
 // Concatenate & Minify JS
 // gulp.task('scripts', ['uglify'] function() {
@@ -103,24 +102,24 @@ gulp.task('uglify', function () {
 
 
 
-// Connect server
-gulp.task('connect', function() {
-  connect.server({
-    port: 9007,
-    root: 'dist'
-  });
-});
+// // Connect server
+// gulp.task('connect', function() {
+//   connect.server({
+//     port: 9007,
+//     root: 'dist'
+//   });
+// });
 
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch(['**/*.sass'], ['sass']);
-    gulp.watch(['**/*.jade', '**/*.pug'], ['pug']);
-    gulp.watch(['js/*.js'], ['lint', 'uglify']);
+    // livereload.listen();
+    // gulp.watch(['**/*.sass'], ['sass']);
+    gulp.watch(['**/*.pug'], ['pug']);
+    // gulp.watch(['js/*.js'], ['lint', 'uglify']);
 });
 
 // Default Task
-gulp.task('default', ['connect', 'sass', 'pug', 'lint', 'uglify', 'watch']);
+gulp.task('default', ['pug', 'watch']);
 
 
